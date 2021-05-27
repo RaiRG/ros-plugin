a5a0292ec90f53762b4cc8891aab6248cb5f9e08
Railina <railina30@yandex.ru>
2021-05-27 14:24:51 +0400

    Fix

---
+++ catkin_ws/src/railina/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/railina/catkin_ws/src/railina
+# Build directory: /home/railina/catkin_ws/src/railina/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
